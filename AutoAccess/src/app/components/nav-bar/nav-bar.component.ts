9a72e4bbe837bc2b777bbaeea17f3468498a76a2
Moldaspan <ersultanmoldaspanov@mail.ru>
2024-12-05 20:54:13 +0500

    nav-bar completed

--- AutoAccess/src/app/components/nav-bar/nav-bar.component.ts
+++ AutoAccess/src/app/components/nav-bar/nav-bar.component.ts
@@ -7,28 +7,41 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit, OnDestroy {
-  logo = '../assets/logo.png'; // Укажи правильный путь к логотипу
-  isLoggedIn = true;
+  logo = '../assets/logo.png'; // Укажите путь к логотипу
+  isLoggedIn = false;
 
   constructor(private router: Router) {}
 
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined';
+  }
+
   ngOnInit() {
-    // window.addEventListener('storage', this.handleStorageChange.bind(this));
+    if (this.isBrowser()) {
+      this.isLoggedIn = !!localStorage.getItem('access_token');
+      window.addEventListener('storage', this.handleStorageChange.bind(this));
+    }
   }
 
   ngOnDestroy() {
-    // window.removeEventListener('storage', this.handleStorageChange.bind(this));
+    if (this.isBrowser()) {
+      window.removeEventListener('storage', this.handleStorageChange.bind(this));
+    }
   }
 
   handleStorageChange() {
-    // this.isLoggedIn = !!localStorage.getItem('access_token');
+    if (this.isBrowser()) {
+      this.isLoggedIn = !!localStorage.getItem('access_token');
+    }
   }
 
   handleLogout() {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
-    localStorage.removeItem('user_id');
-    this.isLoggedIn = false;
-    this.router.navigate(['/login']);
+    if (this.isBrowser()) {
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+      localStorage.removeItem('user_id');
+      this.isLoggedIn = false;
+      this.router.navigate(['/login']);
+    }
   }
 }
